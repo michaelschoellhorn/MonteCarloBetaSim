8ed5b53e51c636d01f8eaf7527c28c6653300f58
michaelschoellhorn <m-schoellhorn@gmx.de>
2023-11-07 18:44:27 +0100

    Make plotheat.py compatible with new file format

--- visualization/plotheat.py
+++ visualization/plotheat.py
@@ -1,16 +1,22 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-X_MAX, Y_MAX, Z_MAX, CELL_SIZE, _ = np.loadtxt("visualization/energyGrid.txt", max_rows=1, unpack=True)
+# get simulation constants
+X_MAX, Y_MAX, Z_MAX, CELL_SIZE, _ = np.loadtxt(
+    "visualization/energyGrid.txt", max_rows=1, skiprows=1, unpack=True)
 
-data = np.loadtxt("visualization/energyGrid.txt", skiprows=1)
-data3D = data.T.reshape((int(Z_MAX/CELL_SIZE), int(X_MAX/CELL_SIZE), int(Y_MAX/CELL_SIZE)))
+# load and modify data
+data = np.loadtxt("visualization/energyGrid.txt", skiprows=3)
+data3D = data.T.reshape(
+    (int(Z_MAX/CELL_SIZE), int(X_MAX/CELL_SIZE), int(Y_MAX/CELL_SIZE)))
 hm = np.sum(data3D, axis=2)
 
-
+# setup figure and plot
 fig, ax = plt.subplots()
-im = ax.imshow(hm, cmap=plt.cm.viridis, origin='lower', extent=(0, X_MAX, 0, Z_MAX), aspect='auto')
-fig.colorbar(im, label='Total E deposited', ax=ax)
-ax.set_xlabel('x')
-ax.set_ylabel('z')
+im = ax.imshow(hm, cmap=plt.cm.viridis, origin='lower',
+               extent=(0, X_MAX, 0, Z_MAX), aspect='auto')
+fig.colorbar(im, label=r'$E_{total}$ in $MeV$', ax=ax)
+ax.set_xlabel(r'$x$')
+ax.set_ylabel(r'$z$')
+ax.set_title(r'energy deposition')
 plt.show()
